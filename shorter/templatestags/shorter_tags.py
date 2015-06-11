880e8c2346ac6d4db6796548aa235dc40f9b112b
Luiz Braga <luizbragaa@gmail.com>
2015-06-11 03:36:49 -0300

    dumb fixes

--- shorter/templatestags/shorter_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def short_it(link):
-    shortener = Shortener('GoogleShortener')
-    return format(shortener.short(self.url))
---
+++ shorter/templatetags/shorter_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from pyshorteners.shorteners  import Shortener
+
+register = template.Library()
+
+@register.simple_tag
+def short_it(link):
+    shortener = Shortener('TinyurlShortener')
+    return shortener.short(link)
